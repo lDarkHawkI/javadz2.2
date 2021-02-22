7eabc8b37f94fcd4d915dcdc368818d5f3384515
lDarkHawkI <lbraini@icloud.com>
2021-02-22 19:34:32 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long Balance = 100;
+        long refill =1220;
+        long bonus = refill / 100;
+        System.out.println(bonus);
+    }
+}
